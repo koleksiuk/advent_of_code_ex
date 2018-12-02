4561d315beb966a7eaf923fa84306ae2f50dd192
Konrad Oleksiuk <konole@gmail.com>
2018-12-02 20:03:01 +0100

    Initial commit

---
+++ 2018/01_chronal_calibration/lib/chronal_calibration.ex
@@ -0,0 +1,18 @@
+defmodule ChronalCalibration do
+  @moduledoc """
+  Documentation for ChronalCalibration.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ChronalCalibration.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ 2018/01_chronal_calibration/test/chronal_calibration_test.exs
@@ -0,0 +1,8 @@
+defmodule ChronalCalibrationTest do
+  use ExUnit.Case
+  doctest ChronalCalibration
+
+  test "greets the world" do
+    assert ChronalCalibration.hello() == :world
+  end
+end
