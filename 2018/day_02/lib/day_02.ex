57ee5a9f90c12b0fa8ea1245fc2fbb5b8d3d0a64
Konrad Oleksiuk <konole@gmail.com>
2018-12-04 22:29:49 +0100

    Day02: Add some more tests

--- 2018/day_02/lib/day_02.ex
+++ 2018/day_02/lib/day_02.ex
@@ -14,6 +14,13 @@ defmodule Day02 do
     |> (fn({doubles, triples}) -> doubles * triples end).()
   end
 
+  def find_key_letters do
+    File.stream!("./var/input.txt")
+    |> Stream.map(&String.trim/1)
+    |> Enum.map(&(&1))
+    |> find_best_matching_string()
+  end
+
   @doc ~S"""
 
   iex> Day02.check("abcdef")
@@ -41,33 +48,88 @@ defmodule Day02 do
     |> fetch_details(string)
   end
 
-  def fetch_details(values, string) do
+  defp fetch_details(values, string) do
     {string, has_double?(values), has_triple?(values)}
   end
 
-  def has_double?(values) do
+  defp has_double?(values) do
     Enum.any?(values, &(&1 == 2))
   end
 
-  def has_triple?(values) do
+  defp has_triple?(values) do
     Enum.any?(values, &(&1 == 3))
   end
 
-  def group_letters(letters) do
+  defp group_letters(letters) do
     group_letters(%{}, letters)
   end
 
-  def group_letters(map, []) do
+  defp group_letters(map, []) do
     map
   end
 
-  def group_letters(map, [letter|letters]) do
+  defp group_letters(map, [letter|letters]) do
     group_letters(map, letter, letters)
   end
 
-  def group_letters(map, letter, letters) do
+  defp group_letters(map, letter, letters) do
     count = Map.get(map, letter, 0)
 
     group_letters(Map.put(map, letter, count + 1), letters)
+  end
+
+  defp find_best_matching_string([]) do
+    nil
+  end
+
+  defp find_best_matching_string([string|strings]) do
+    case find_similar(string, strings) do
+      nil -> find_best_matching_string(strings)
+      matched_string -> get_matched_letters(string, matched_string)
+    end
+  end
+
+  @doc ~S"""
+
+  iex> Day02.find_similar("abcd", ["accc", "abba", "aacd"])
+  "aacd"
+
+  iex> Day02.find_similar("abcd", ["accc", "abba", "abce"])
+  "abce"
+
+  iex> Day02.find_similar("abcd", ["abee"])
+  nil
+
+  iex> Day02.find_similar("abcd", ["accc", "aaaa", "dcba"])
+  nil
+  """
+  def find_similar(string, strings) do
+    Enum.find(strings, fn (str) ->
+      similar_letters = get_matched_letters(string, str) |> String.length
+
+      similar_letters == String.length(string) - 1
+    end)
+  end
+
+  @doc ~S"""
+
+  iex> Day02.get_matched_letters("abcd", "abce")
+  "abc"
+
+  iex> Day02.get_matched_letters("abcdefg", "abcdefg")
+  "abcdefg"
+
+  iex> Day02.get_matched_letters("abdefg", "abcdefg")
+  "abdefg"
+
+  iex> Day02.get_matched_letters("abdd", "abcd")
+  "abd"
+
+  """
+
+  def get_matched_letters(string, str) do
+    String.myers_difference(string, str)
+    |> Keyword.get_values(:eq)
+    |> Enum.join
   end
 end
